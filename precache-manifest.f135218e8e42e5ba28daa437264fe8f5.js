4eec5718cdac95dd07dbc0f9168ff08529732022
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2019-12-14 22:30:21 +0530

    Updates

---
+++ precache-manifest.f135218e8e42e5ba28daa437264fe8f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826cf0161289ee925e112443326c7e88",
+    "url": "/form-management/index.html"
+  },
+  {
+    "revision": "f10d3b5439ad88310900",
+    "url": "/form-management/static/css/main.bb164b0b.chunk.css"
+  },
+  {
+    "revision": "6f441385447d92a3fa1d",
+    "url": "/form-management/static/js/2.089e6f19.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/form-management/static/js/2.089e6f19.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f10d3b5439ad88310900",
+    "url": "/form-management/static/js/main.d161081c.chunk.js"
+  },
+  {
+    "revision": "2313cc7a892e7e526435",
+    "url": "/form-management/static/js/runtime-main.038ce6a7.js"
+  }
+]);
